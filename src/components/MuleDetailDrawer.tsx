b5fccb5c2eb0eec1c1d8eac01f01e9633f475a6d
Test <test@test.com>
2026-04-28 20:24:20 -0700

    update padding

--- src/components/MuleDetailDrawer.tsx
+++ src/components/MuleDetailDrawer.tsx
@@ -240,7 +240,7 @@ export function MuleDetailDrawer({
 
             <div className="mx-6 border-t border-border/50" />
 
-            <div className="p-6 flex flex-col gap-5 max-sm:pb-16">
+            <div className="p-6 flex flex-col gap-5 max-sm:pb-7">
               <MuleIdentityFields mule={mule} identity={identity} onUpdate={onUpdate} />
 
               <div>
